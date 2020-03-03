c9feb04c50d7ee526b50659de3b777031c4ea46c
xenixz <kocurek.pavel@gmail.com>
2020-03-03 23:06:45 +0100

    vowels or silent consonants at start

--- translate.php
+++ translate.php
@@ -1,6 +1,24 @@
 <?php
-$input = $_POST['input'];
+$input = strtolower($_POST['input']);	//load input, change to lowercase
+
 if (!empty($input)) {
-	echo $_POST['input'];
+	$vowels = array("a", "e", "i", "o", "u");
+	$silentCons = array("p", "k", "h", "g", "w"); //phone, knife, honest, gnome, whole
+	$validChars = array_merge($vowels, $silentCons);
+	$justAddEy = false;		//determines, if input word needs cutting
+	$suffix = "ay";		
+	$result = "-";	//initialize empty value
+
+	foreach($validChars as $char){	//check first char of input
+		if ($char == $input[0]) {
+			$justAddEy = true;
+		}
+	}
+
+	if ($justAddEy) {
+		$result = $input . $suffix;
+	}
+
+	echo $result;	//print translated word
 }else echo "-";
 ?>
