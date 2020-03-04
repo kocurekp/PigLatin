b69c6c67d7b29a1669c8adcf7706723c6ba39dda
xenixz <kocurek.pavel@gmail.com>
2020-03-04 01:34:21 +0100

    working prototype

--- translate.php
+++ translate.php
@@ -6,9 +6,11 @@ if (!empty($input)) {
 	$vowels = array("a", "e", "i", "o", "u");
 	$silentWords = array();
 	$justAddEy = false;		//determines, if input word needs cutting
-	$suffix = "'way";		
+	$addSuffix = "'way";
+	$counter = 0;	
+	$prefix = "";
 
-	$file = fopen("silentWords.txt","r");	//load silent words from file
+	$file = fopen("silentWords.txt","r");	//load silent words from file, this is not pretty implementation, but as far as I know, there is not an easy way to determine when consonants are silent
 	while(! feof($file))
 	{
 		$word = fgets($file, 4096);
@@ -28,7 +30,22 @@ if (!empty($input)) {
 	}
 
 	if ($justAddEy) {
-		$result = $input . $suffix;
+		$result = $input . $addSuffix;
+	}else{
+		$prefix = $input[0];
+		for ($i=1; $i < strlen($input); $i++) { 
+			foreach($vowels as $char){	//check first char of input
+				if ($char == $input[$i]) {
+					if ($input[$i] == "u" && $prefix == "q") {	//qu is exception 
+						$prefix = $prefix . $input[$i];
+						$i++;
+					}
+					$result = substr($input, $i) . "-" . $prefix . "ay";
+					break 2;
+				}
+			}
+			$prefix = $prefix . $input[$i];
+		}
 	}
 
 	echo $result;	//print translated word
