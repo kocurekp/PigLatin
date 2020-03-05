526c046735f6a950598289c83e3b01d6cf798754
xenixz <kocurek.pavel@gmail.com>
2020-03-05 17:50:34 +0100

    now supports multiple words

--- translate.php
+++ translate.php
@@ -1,53 +1,75 @@
 <?php
 $input = strtolower($_POST['input']);	//load input, change to lowercase
-$result = "-";	//initialize empty value
+$output = "";	//initialize empty value
 
-if (!empty($input)) {
-	$vowels = array("a", "e", "i", "o", "u");
-	$silentWords = array();
-	$justAddEy = false;		//determines, if input word needs cutting
-	$addSuffix = "'way";
-	$counter = 0;	
-	$prefix = "";
 
-	$file = fopen("silentWords.txt","r");	//load silent words from file, this is not pretty implementation, but as far as I know, there is not an easy way to determine when consonants are silent
+function LoadSilentWords(&$silentWords){
+	$file = fopen("silentWords.txt","r");	//load silent words from file, there is not an easy way to determine when consonants are silent
 	while(! feof($file))
 	{
 		$word = fgets($file, 4096);
 		array_push($silentWords, $word);	//store into array
 	}
+}
 
+function CheckFirstChar(&$silentWords, &$vowels, $input){
 	foreach($silentWords as $item){		//check usual words that starts with silent letter
 		if (strcmp($item, $input) == 2) {
-			$justAddEy = true;
+			return true;
 		}
 	}
 
 	foreach($vowels as $char){	//check first char of input
 		if ($char == $input[0]) {
-			$justAddEy = true;
+			return true;
 		}
 	}
+	return false;
+}
 
-	if ($justAddEy) {
-		$result = $input . $addSuffix;
-	}else{
-		$prefix = $input[0];
-		for ($i=1; $i < strlen($input); $i++) { 
-			foreach($vowels as $char){	//check first char of input
-				if ($char == $input[$i]) {
-					if ($input[$i] == "u" && $prefix == "q") {	//qu is exception 
-						$prefix = $prefix . $input[$i];
-						$i++;
+
+// print_r($space);
+
+if (!empty($input)) {
+
+	$multipleInput = explode(" ", $input);
+	// echo(count($multipleInput));
+	foreach($multipleInput as $input){
+
+		// echo $input;
+
+		$silentWords = array();
+		$vowels = array("a", "e", "i", "o", "u");
+		$hasVowel = false;
+		$suffix = "'way";
+		$counter = 0;	
+		$prefix = "";
+
+		LoadSilentWords($silentWords);
+		if (CheckFirstChar($silentWords, $vowels, $input)) { //determines, if input word needs cutting
+			$result = $input . $suffix;
+		}else{
+			$prefix = $input[0];
+			for ($i=1; $i < strlen($input); $i++) { 
+				foreach($vowels as $char){	//check first char of input
+					if ($char == $input[$i]) {
+						$hasVowel = true;
+						if ($input[$i] == "u" && $prefix == "q") {	//qu is exception 
+							$prefix = $prefix . $input[$i];
+							$i++;
+						}
+						$result = substr($input, $i) . "-" . $prefix . "ay";
+						break 2;
 					}
-					$result = substr($input, $i) . "-" . $prefix . "ay";
-					break 2;
 				}
+				if (!$hasVowel) {
+					$result = $input . $suffix;
+				}
+				$prefix = $prefix . $input[$i];
 			}
-			$prefix = $prefix . $input[$i];
 		}
+		$output = $output . " " . $result;
 	}
-
-	echo $result;	//print translated word
-}else echo $result;
-?>
+}
+	echo $output;	//print translated word
+	?>
